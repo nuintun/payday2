3bddd041a7b93b7a6bcbe33ef5a3b51b19bf8822
nuintun <nuintun@gmail.com>
2016-01-07 14:18:08 +0800

    add files

--- mods/reconnect/FuckLostConnection.lua
+++ mods/reconnect/FuckLostConnection.lua
@@ -110,7 +110,7 @@ if RequiredScript == "lib/managers/crimenetmanager" then
       if Reconnect.options.room_id then
         managers.network.matchmake:join_server(Reconnect.options.room_id)
       else
-        QuickMenu:new("错误！", "未找到可以连接的服务器。", {}):show()
+        QuickMenu:new("错误！", "未找到可以连接的服务器。", { [1] = { text = "确认" } }):show()
       end
       return
     end
