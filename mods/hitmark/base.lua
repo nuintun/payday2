ad2933fa38e64012feb5830ec1eb99539c083907
nuintun <nuintun@gmail.com>
2016-01-27 00:00:36 +0800

    update files

--- mods/hitmark/base.lua
+++ mods/hitmark/base.lua
@@ -5,32 +5,17 @@ HitMark.ModPath = ModPath
 HitMark.settings = {
   hit_texture = "guis/textures/pd2/hitconfirm",
   crit_texture = "guis/textures/pd2/hitconfirm_crit",
-  headshot_texture = "guis/textures/pd2/hitconfirm_headshot",
   hit = "ff0000",
-  crit = "ff0000",
+  crit = "ffff00",
   headshot = "ff0000",
-  hit_kill = "00ff00",
-  crit_kill = "00ff00",
-  headshot_kill = "00ff00",
   blend_mode = "normal"
 }
-
-function HitMark:custom_texture_loaded()
-  --TODO custom texture loaded
-end
-
-function HitMark:load()
-  TextureCache:request(HitMark.settings.headshot_texture, "NORMAL", callback(self, self, "custom_texture_loaded"), 100)
-end
-
-HitMark:load()
 
 if RequiredScript then
   local hook_files = {
     ["lib/managers/hudmanagerpd2"] = "lib/hudmanagerpd2.lua",
     ["lib/managers/hud/hudhitconfirm"] = "lib/hudhitconfirm.lua",
-    ["lib/units/enemies/cop/copdamage"] = "lib/copdamage.lua",
-    ["lib/units/equipment/sentry_gun/sentrygundamage"] = "lib/sentrygundamage.lua"
+    ["lib/units/enemies/cop/copdamage"] = "lib/copdamage.lua"
   }
   local requiredScript = RequiredScript:lower()
 
