4d8de9720ae33462776e49f42e00c4c3fa720f41
nuintun <nuintun@gmail.com>
2016-01-27 10:51:06 +0800

    update

--- mods/hitmark/lib/copdamage.lua
+++ mods/hitmark/lib/copdamage.lua
@@ -14,16 +14,10 @@ end
 -- @param self
 -- @param attack_data
 local function PostHook(self, attack_data)
-  if not attack_data.attacker_unit
-    or attack_data.attacker_unit
-    and attack_data.attacker_unit ~= managers.player:player_unit() then
-    return
-  end
-
   if HitMark.direct_hit then
     local death = attack_data.result.type == "death"
     local body = attack_data.body_name or attack_data.col_ray.body:name()
-    local body_key = body:key()
+    local body_key = body and body:key()
     local headshot = false
 
     if body_key then
@@ -46,41 +40,8 @@ Hooks:PreHook(CopDamage, "damage_tase", "hitmark_copdamage_tase_pre", PreHook)
 Hooks:PreHook(CopDamage, "damage_melee", "hitmark_copdamage_melee_pre", PreHook)
 
 --- 监听函数挂接后置钩子
-Hooks:PostHook(CopDamage, "damage_bullet", "hitmark_copdamage_bullet", function(self, attack_data)
-  HitMark.hooked = false
-
-  if self._dead or self._invulnerable then return end
-  if PlayerDamage.is_friendly_fire(self, attack_data.attacker_unit) then return end
-
-  PostHook(self, attack_data)
-end)
-Hooks:PostHook(CopDamage, "damage_fire", "hitmark_copdamage_fire", function(self, attack_data)
-  HitMark.hooked = false
-
-  if self._dead then return end
-
-  PostHook(self, attack_data)
-end)
-Hooks:PostHook(CopDamage, "damage_explosion", "hitmark_copdamage_explosion", function(self, attack_data)
-  HitMark.hooked = false
-
-  if self._dead or self._invulnerable then return end
-
-  PostHook(self, attack_data)
-end)
-Hooks:PostHook(CopDamage, "damage_tase", "hitmark_copdamage_tase", function(self, attack_data)
-  HitMark.hooked = false
-
-  if self._dead or self._invulnerable then return end
-  if PlayerDamage.is_friendly_fire(self, attack_data.attacker_unit) then return end
-
-  PostHook(self, attack_data)
-end)
-Hooks:PostHook(CopDamage, "damage_melee", "hitmark_copdamage_melee", function(self, attack_data)
-  HitMark.hooked = false
-
-  if self._dead or self._invulnerable then return end
-  if PlayerDamage.is_friendly_fire(self, attack_data.attacker_unit) then return end
-
-  PostHook(self, attack_data)
-end)
+Hooks:PostHook(CopDamage, "damage_bullet", "hitmark_copdamage_bullet", PostHook)
+Hooks:PostHook(CopDamage, "damage_fire", "hitmark_copdamage_fire", PostHook)
+Hooks:PostHook(CopDamage, "damage_explosion", "hitmark_copdamage_explosion", PostHook)
+Hooks:PostHook(CopDamage, "damage_tase", "hitmark_copdamage_tase", PostHook)
+Hooks:PostHook(CopDamage, "damage_melee", "hitmark_copdamage_melee", PostHook)
