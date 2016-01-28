fae84f76a56c80b8a1acded6c6642a760f81c1d3
nuintun <nuintun@gmail.com>
2016-01-28 17:35:33 +0800

    update

--- mods/kill_counter/lib/hudteammate.lua
+++ mods/kill_counter/lib/hudteammate.lua
@@ -1,44 +1,47 @@
 Hooks:PostHook(HUDTeammate, "init", "killcounter_hudteammate_init", function(self, i, teammates_panel)
   local kill_counter_name = "kill_counter_" .. i
+  local main_player = i == HUDManager.PLAYER_PANEL
   local teammate_panel = teammates_panel:child("" .. i)
 
-  if teammates_panel:child(kill_counter_name) then
-    teammates_panel:remove(teammates_panel:child(kill_counter_name))
+  if main_player then
+    if teammates_panel:child(kill_counter_name) then
+      teammates_panel:remove(teammates_panel:child(kill_counter_name))
+    end
+    
+    self._kill_counter = self._hud_panel:panel({
+      name = kill_counter_name,
+      visible = true,
+      w = teammate_panel:w(),
+      h = KillCounter.height,
+      x = 0,
+      halign = "right"
+    })
+
+    self._kill_icon = self._kill_counter:bitmap({
+      layer = 1,
+      name = "kill_icon",
+      texture = "guis/textures/pd2/risklevel_blackscreen",
+      w = self._kill_counter:h(),
+      h = self._kill_counter:h(),
+      blend_mode = KillCounter.blend_mode,
+      color = Color(KillCounter.color)
+    })
+
+    self._kill_text = self._kill_counter:text({
+      layer = 2,
+      name = "kill_text",
+      text = "0/0",
+      w = self._kill_counter:w() - self._kill_icon:w(),
+      h = self._kill_counter:h(),
+      align = "right",
+      vertical = "center",
+      color = Color(KillCounter.color),
+      font = "fonts/font_medium_mf",
+      font_size = self._kill_counter:h()
+    })
+
+    self._kill_counter.set_righttop(teammate_panel:right(), teammate_panel:bottom())
   end
-
-  self._kill_counter = self._hud_panel:panel({
-    name = kill_counter_name,
-    visible = true,
-    w = teammate_panel:w(),
-    h = KillCounter.height,
-    x = 0,
-    halign = "right"
-  })
-
-  self._kill_icon = self._kill_counter:bitmap({
-    layer = 1,
-    name = "kill_icon",
-    texture = "guis/textures/pd2/risklevel_blackscreen",
-    w = self._kill_counter:h(),
-    h = self._kill_counter:h(),
-    blend_mode = KillCounter.blend_mode,
-    color = Color(KillCounter.color)
-  })
-
-  self._kill_text = self._kill_counter:text({
-    layer = 2,
-    name = "kill_text",
-    text = "0/0",
-    w = self._kill_counter:w() - self._kill_icon:w(),
-    h = self._kill_counter:h(),
-    align = "right",
-    vertical = "center",
-    color = Color(KillCounter.color),
-    font = "fonts/font_medium_mf",
-    font_size = self._kill_counter:h()
-  })
-
-  self._kill_counter.set_righttop(teammate_panel:right(), teammate_panel:bottom())
 end)
 
 function HUDTeammate:update_kill_counter(headshot, total)
--- mods/kill_counter/lib/statisticsmanager.lua
+++ mods/kill_counter/lib/statisticsmanager.lua
@@ -1,11 +1,6 @@
-Hooks:PostHook(StatisticsManager, "killed", "killcounter_statisticsmanager_killed", function(self, data)
-  local name_id, throwable_id = self:_get_name_id_and_throwable_id(data.weapon_unit)
+Hooks:PostHook(StatisticsManager, "killed", "killcounter_statisticsmanager_killed", function(self)
+  local headshot = self._global.session.killed.total.head_shots or 0
+  local total = self._global.session.killed.total.count or 0
 
-  if data.variant == "melee" then
-    managers.hud:update_kill_counter(self._global.session.killed.total.melee or 0, self._global.session.killed.total.count or 0)
-  elseif not throwable_id then
-    managers.hud:update_kill_counter(self._global.session.killed_by_weapon[name_id] and self._global.session.killed_by_weapon[name_id].count or 0, self._global.session.killed.total.count or 0)
-  end
-
-  self._teammate_panels[HUDManager.PLAYER_PANEL]:update_kill_counter(weapon, total)
+  managers.hud:update_kill_counter(headshot, total)
 end)
