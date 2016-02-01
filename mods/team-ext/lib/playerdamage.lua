258998e5c3a2e6057c8588c75b17e608fecac2c2
nuintun <nuintun@gmail.com>
2016-02-01 16:10:02 +0800

    update

--- mods/team-ext/lib/playerdamage.lua
+++
@@ -1,8 +0,0 @@
---noinspection UnusedDef
-Hooks:PostHook(PlayerDamage, "update", "teamExtPlayerDamageUpdate", function(self, unit, t, dt)
-  if self:got_messiah_charges() then
-    managers.hud:update_downed_counter(self._messiah_charges .. "/" .. tostring(Application:digest_value(self._revives, false) - 1))
-  else
-    managers.hud:update_downed_counter(tostring(Application:digest_value(self._revives, false) - 1))
-  end
-end)
--- mods/team-ext/lib/unitnetworkhandler.lua
+++
@@ -1,9 +0,0 @@
---noinspection UnusedDef
-Hooks:PostHook(UnitNetworkHandler, "sync_doctor_bag_taken", "teamExtUnitNetworkHandlerSyncDoctorBagTaken", function(self, unit, amount, sender)
-  local peer = self._verify_sender(sender)
-  local peer_id = peer and peer:id()
-
-  if not peer_id then return end
-
-  managers.hud:sync_doctor_bag_taken(managers.hud:get_panel_by_peer_id(peer_id))
-end)
