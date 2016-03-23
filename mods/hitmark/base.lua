25158db105139de008ca5b29619286dd225417c1
nuintun <nuintun@gmail.com>
2016-03-23 23:26:12 +0800

    update files

--- mods/hitmark/base.lua
+++ mods/hitmark/base.lua
@@ -15,7 +15,7 @@ HitMark.settings = {
   hit_texture = "guis/textures/pd2/hitconfirm",
   kill_texture = "guis/textures/pd2/hitconfirm_crit",
   hit = "ff5500",
-  crit = "ff00ff",
+  crit = "ff0000",
   headshot = "57ff00",
   blend_mode = "add"
 }
